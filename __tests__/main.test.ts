9acad11512eb002abbc7bd4f1d4f21de6ef66dd3
grassedge <grassedge@gmail.com>
2019-12-21 01:29:15 +0900

    test updatedFiles()

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,4 +1,4 @@
-import { retrieveCodes } from '../src/utils'
+import { retrieveCodes, updatedFiles } from '../src/utils'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
@@ -41,3 +41,43 @@ B -> C: test3
         }
     ]);
 });
+
+test('updatedFiles', async() => {
+    const files = await updatedFiles(octokitMock, pushEventPayloadMock);
+    await expect(files).toEqual([ 'action.yml', '' ]);
+});
+
+const octokitMock = {
+    git: {
+        getCommit: async function(opts) {
+            switch(opts.commit_sha) {
+                case '8bcd2e375cabcfe6fabd35f8685d13c48c2d09d0':
+                    return { data: commitMock };
+                default:
+                    throw new Error('');
+            }
+        }
+    }
+}
+
+const pushEventPayloadMock = {
+    "commits": [
+        {
+            "id": "8bcd2e375cabcfe6fabd35f8685d13c48c2d09d0"
+        }
+    ],
+    "repository": {
+        "name": "Hello-World",
+        "owner": {
+            "login": "Codertocat",
+        }
+    }
+};
+
+const commitMock = {
+    "parents": [
+        {
+            "sha": "e1127d04c7fb5730692b09eea14f34d60598577b"
+        }
+    ]
+};
